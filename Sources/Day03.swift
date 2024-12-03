04bee0e3cdf7d36d105cdc1a50fb32797b8f8a48
Evan <evan@brucemcrooster.dev>
2024-12-03 12:10:53 -0500

    Complete Day 3 Part 2 Moved summing multiples to a separate function, which part1 now uses

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -4,7 +4,11 @@ struct Day03: AdventDay {
   var data: String
 
   func part1() -> Any {
-    let matches = data.matches(of: /mul\((?<num1>\d+),(?<num2>\d+)\)/)
+    return sumOfMults(in: data)
+  }
+  
+  func sumOfMults(in checkData: String) -> Int {
+    let matches = checkData.matches(of: /mul\((?<num1>\d+),(?<num2>\d+)\)/)
     
     var sum = 0
     
@@ -15,6 +19,44 @@ struct Day03: AdventDay {
   }
 
   func part2() -> Any {
-    return 0
+    let controlStatements = data.matches(of: /do(n't)?\(\)/)
+    
+    if controlStatements.isEmpty {
+      return 0
+    }
+    
+    var sum = sumOfMults(
+      in: String(data[data.startIndex..<controlStatements.first!.range.lowerBound])
+    )
+    
+    var matchIndex = controlStatements.startIndex
+    
+    while matchIndex < controlStatements.endIndex {
+      let match = controlStatements[matchIndex]
+      
+      if match.output.1 == nil { // Matched a "do" (without "n't")
+        let nextDontIndex = controlStatements.firstIndex { otherMatch in
+          otherMatch.range.lowerBound > match.range.upperBound
+            && otherMatch.output.1 != nil // Matches a "don't"
+        }
+        
+        let endSubstringIndex = if nextDontIndex != nil {
+          controlStatements[nextDontIndex!].range.lowerBound
+        } else { data.endIndex }
+        
+        let nextSubstring: Substring = data[match.range.upperBound..<endSubstringIndex]
+        
+        sum += sumOfMults(in: String(nextSubstring))
+        
+        if nextDontIndex != nil {
+          matchIndex = nextDontIndex!
+        } else {
+          matchIndex = controlStatements.endIndex
+        }
+      }
+      matchIndex += 1
+    }
+    
+    return sum
   }
 }
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -10,7 +10,9 @@ struct Day03Tests {
     #expect(String(describing: Day03(data: testData).part1()) == "161")
   }
 
+  let otherTestData = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+  
   @Test func testPart2() async throws {
-    #expect(true)
+    #expect(String(describing: Day03(data: otherTestData).part2()) == "48")
   }
 }

1c9c9178816b6e6df42e50bc574614cfcdbe9b03
Evan <evan@brucemcrooster.dev>
2024-12-07 08:07:15 -0500

    Completed Day 7 Part 1

---
+++ Sources/Day07.swift
@@ -0,0 +1,88 @@
+import Algorithms
+
+struct Day07: AdventDay {
+  var data: String
+  
+  var callibrations: [Calibration]
+  
+  init(data: String) {
+    self.data = data
+    self.callibrations = data
+      .split(separator: "\n")
+      .filter({!$0.isEmpty})
+      .map({Calibration(fromLine: String($0))})
+  }
+  
+  struct Calibration {
+    let sum: Int
+    let values: [Int]
+    
+    init(fromLine line: String) {
+      let split = line.split(separator: ": ", maxSplits: 2)
+      
+      self.sum = Int(split.first!)!
+      self.values = split.last!
+        .split(separator: " ")
+        .map({ Int($0)! })
+    }
+  }
+
+  enum Operator {
+    case add, multiply
+    
+    func operate(_ a: Int, _ b: Int) -> Int {
+      switch self {
+      case .add: return a + b
+      case .multiply: return a * b
+      }
+    }
+  }
+  
+  func part1() -> Any {
+    func canWork(_ calibration: Calibration) -> Bool {
+      var withoutFirst: [Int] = calibration.values
+      withoutFirst.removeFirst()
+      
+      for numMultiplies in 0...withoutFirst.count {
+        let operators: [Operator] = Array(
+          repeating: .multiply,
+          count: numMultiplies
+        ) + Array(
+          repeating: .add,
+          count: withoutFirst.count - numMultiplies
+        )
+        
+        assert(operators.count == withoutFirst.count)
+        
+        
+        
+        for opSet in operators.uniquePermutations() {
+          var result = calibration.values.first!
+          for (`operator`, num) in zip(opSet, withoutFirst) {
+            result = `operator`.operate(result, num)
+          }
+          if result == calibration.sum {
+            return true
+          }
+        }
+        
+        
+      }
+      return false
+    }
+    
+    var sum = 0
+    
+    for callibration in callibrations {
+      if canWork(callibration) {
+        sum += callibration.sum
+      }
+    }
+    
+    return sum
+  }
+
+  func part2() -> Any {
+    return 0
+  }
+}
---
+++ Tests/Day07.swift
@@ -0,0 +1,30 @@
+import Testing
+
+@testable import AdventOfCode
+
+struct Day07Tests {
+
+  let testData = """
+  190: 10 19
+  3267: 81 40 27
+  83: 17 5
+  156: 15 6
+  7290: 6 8 6 15
+  161011: 16 10 13
+  192: 17 8 14
+  21037: 9 7 18 13
+  292: 11 6 16 20
+  
+  """
+
+  @Test func testPart1() async throws {
+    let day07 = Day07(data: testData)
+    
+    #expect(day07.callibrations.count == 9)
+    #expect(String(describing: day07.part1()) == "3749")
+  }
+
+  @Test func testPart2() async throws {
+    #expect(true)
+  }
+}

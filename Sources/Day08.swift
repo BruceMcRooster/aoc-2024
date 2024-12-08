a27034c576e640cd156eace20321b4df9491afd4
Evan <evan@brucemcrooster.dev>
2024-12-08 09:48:03 -0500

    Day 8 Part 1

---
+++ Sources/Day08.swift
@@ -0,0 +1,126 @@
+import Algorithms
+
+struct Day08: AdventDay {
+  var data: String
+  
+  let board: [Character]
+  let width: Int
+  let height: Int
+  
+  init(data: String) {
+    self.data = data
+    self.width = data.split(separator: "\n").first!.count
+    self.height = data.split(separator: "\n").filter { !$0.isEmpty }.count
+    self.board = {
+      let lines = data.split(separator: "\n").filter { !$0.isEmpty }
+      return lines.reduce([]) { arr, line in
+        arr + Array(line).filter { $0 != "\n" }
+      }
+    }()
+    assert(board.count == width * height)
+  }
+  
+  struct BoardPosition: Hashable, Equatable {
+    let x: Int
+    let y: Int
+    
+    private let width: Int
+    
+    var index: Int {
+      return x + y * width
+    }
+    
+    init(x: Int, y: Int, width: Int) {
+      self.x = x
+      self.y = y
+      self.width = width
+    }
+    
+    init(index: Int, width: Int) {
+      self.x = index % width
+      self.y = index / width
+      self.width = width
+    }
+    
+    static func == (lhs: BoardPosition, rhs: BoardPosition) -> Bool {
+      guard lhs.width == rhs.width else {
+        fatalError("Compared board positions with differing width")
+      }
+      return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+    
+    func hash(into hasher: inout Hasher) {
+      hasher.combine(x)
+      hasher.combine(y)
+    }
+  }
+  
+  func possibleAntinodePositions(_ first: BoardPosition, _ second: BoardPosition) -> [BoardPosition] {
+    let xOffset = second.x - first.x
+    let yOffset = second.y - first.y
+    
+    return [
+      BoardPosition(x: first.x - xOffset, y: first.y - yOffset, width: width),
+      BoardPosition(x: second.x + xOffset, y: second.y + yOffset, width: width)
+    ]
+  }
+  
+  func allPossibleAntinodePositions(_ positions: [BoardPosition]) -> [BoardPosition] {
+    var resultantPositions = [BoardPosition]()
+    for i in 0..<(positions.endIndex - 1) {
+      for j in i+1..<positions.endIndex {
+        resultantPositions += possibleAntinodePositions(positions[i], positions[j])
+      }
+    }
+    return resultantPositions.filter(isOnBoard)
+  }
+  
+  func printBoardWithAntinodes(_ positions: [BoardPosition]) {
+    var stringBoardCopy = data
+    for position in positions {
+      let stringIndex: String.Index = stringBoardCopy.index(
+        stringBoardCopy.startIndex,
+        offsetBy: position.x + (position.y * (width + 1))
+      )
+      
+      if stringBoardCopy[stringIndex] == "." {
+        stringBoardCopy.replaceSubrange(stringIndex...stringIndex, with: "#")
+      }
+    }
+    print(stringBoardCopy)
+  }
+  
+  func isOnBoard(_ position: BoardPosition) -> Bool {
+    position.x >= 0 && position.x < width && position.y >= 0 && position.y < height
+  }
+  
+  func allPositionsOf(_ character: Character) -> [BoardPosition] {
+    var result = [BoardPosition]()
+    for (index, char) in board.enumerated() {
+      if char == character {
+        result.append(BoardPosition(index: index, width: width))
+      }
+    }
+    return result
+  }
+  
+  func part1() -> Any {
+    var foundSet = Set<Character>()
+    var antinodePositions = [BoardPosition]()
+    
+    
+    for char in board {
+      if char != "." && !foundSet.insert(char).inserted {
+        antinodePositions += allPossibleAntinodePositions(allPositionsOf(char))
+      }
+    }
+    
+    let antinodePositionsSet = Set(antinodePositions)
+    
+    return antinodePositionsSet.count
+  }
+
+  func part2() -> Any {
+    return 0
+  }
+}

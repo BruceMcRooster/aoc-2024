4ed5f0d5882dd39b971c268643da3fc86643534c
Evan <evan@brucemcrooster.dev>
2024-12-12 14:02:36 -0500

    Completed Day 12 Part 2

--- Sources/Day12.swift
+++ Sources/Day12.swift
@@ -4,11 +4,17 @@ import DequeModule
 struct Day12: AdventDay {
   
   var data: String
+  
+  let map: [Character]
+  let width: Int
 
+  init(data: String) {
+    self.data = data
+    self.map = data.filter { $0 != "\n" }
+    self.width = data.distance(from: data.startIndex, to: data.firstIndex(of: "\n")!)
+  }
+  
   func part1() -> Any {
-    let map: [Character] = data.filter { $0 != "\n" }
-    let width = data.distance(from: data.startIndex, to: data.firstIndex(of: "\n")!)
-    
     var checked = Set<Int>()
     
     func check(_ index: Int) -> (area: Int, perimeter: Int) {
@@ -74,6 +80,114 @@ struct Day12: AdventDay {
   }
 
   func part2() -> Any {
-    return 0
+    var checked = Set<Int>()
+    
+    func check(from index: Int) -> (area: Int, sideCount: Int) {
+      enum Sides: Hashable {
+        case above(row: Int, column: Int), below(row: Int, column: Int)
+        case left(row: Int, column: Int), right(row: Int, column: Int)
+        
+        func toString() -> String {
+          switch self {
+          case .above(row: let row, column: let column): return "above(\(row), \(column))"
+          case .below(row: let row, column: let column): return "below(\(row), \(column))"
+          case .left(row: let row, column: let column): return "left(\(row),\(column))"
+          case .right(row: let row, column: let column): return "right(\(row),\(column))"
+          }
+        }
+      }
+      
+      var area = 0
+      var sides = Set<Sides>()
+      
+      var nextChecks: Deque<Int> = [index]
+      
+      while !nextChecks.isEmpty {
+        let checkIndex = nextChecks.removeFirst()
+        
+        guard checked.insert(checkIndex).inserted else { continue }
+        
+        area += 1
+        
+        if checkIndex % width == 0 ||
+          map[checkIndex - 1] != map[checkIndex]
+        {
+          sides.insert(.left(row: checkIndex / width, column: checkIndex % width))
+        }
+        if checkIndex % width == width - 1 ||
+          map[checkIndex + 1] != map[checkIndex]
+        {
+          sides.insert(.right(row: checkIndex / width, column: checkIndex % width))
+        }
+        if checkIndex / width == 0 ||
+          map[checkIndex - width] != map[checkIndex]
+        {
+          sides.insert(.above(row: checkIndex / width, column: checkIndex % width))
+        }
+        if checkIndex / width == width - 1 ||
+          map[checkIndex + width] != map[checkIndex]
+        {
+          sides.insert(.below(row: checkIndex / width, column: checkIndex % width))
+        }
+        
+        if checkIndex % width > 0,
+          map[checkIndex - 1] == map[checkIndex],
+          !checked.contains(checkIndex - 1)
+        {
+          nextChecks.append(checkIndex - 1)
+        }
+        if checkIndex % width < width - 1,
+          map[checkIndex + 1] == map[checkIndex],
+          !checked.contains(checkIndex + 1)
+        {
+          nextChecks.append(checkIndex + 1)
+        }
+        if checkIndex / width > 0,
+          map[checkIndex - width] == map[checkIndex],
+          !checked.contains(checkIndex - width)
+        {
+          nextChecks.append(checkIndex - width)
+        }
+        if checkIndex / width < width - 1,
+          map[checkIndex + width] == map[checkIndex],
+          !checked.contains(checkIndex + width)
+        {
+          nextChecks.append(checkIndex + width)
+        }
+      }
+      
+      var sideCount = 0
+            
+      for side in sides {
+        switch side {
+        case .above(
+          row: let row,
+          column: let col
+        ): sideCount += (sides.contains(.above(row: row, column: col - 1))) ? 0 : 1
+        case .below(
+          row: let row,
+          column: let col
+        ): sideCount += (sides.contains(.below(row: row, column: col - 1))) ? 0 : 1
+        case .left(
+          row: let row,
+          column: let col
+        ): sideCount += (sides.contains(.left(row: row - 1, column: col))) ? 0 : 1
+        case .right(
+          row: let row,
+          column: let col
+        ): sideCount += (sides.contains(.right(row: row - 1, column: col))) ? 0 : 1
+        }
+      }
+      
+      return (area, sideCount)
+    }
+    
+    var sum = 0
+    
+    for unchecked in map.indices where !checked.contains(unchecked) {
+      let (area, perimeter) = check(from: unchecked)
+      sum += (area * perimeter)
+    }
+    return sum
   }
 }
--- Tests/Day12.swift
+++ Tests/Day12.swift
@@ -34,6 +34,25 @@ struct Day12Tests {
   MMMISSJEEE
   
   """
+  
+  let ETest = """
+  EEEEE
+  EXXXX
+  EEEEE
+  EXXXX
+  EEEEE
+  
+  """
+  
+  let ABBATest = """
+  AAAAAA
+  AAABBA
+  AAABBA
+  ABBAAA
+  ABBAAA
+  AAAAAA
+  
+  """
 
   @Test func testPart1() async throws {
     #expect(String(describing: Day12(data: smallTest1).part1()) == "140")
@@ -42,6 +61,10 @@ struct Day12Tests {
   }
 
   @Test func testPart2() async throws {
-    #expect(true)
+    #expect(String(describing: Day12(data: smallTest1).part2()) == "80")
+    #expect(String(describing: Day12(data: smallTest2).part2()) == "436")
+    #expect(String(describing: Day12(data: ETest).part2()) == "236")
+    #expect(String(describing: Day12(data: ABBATest).part2()) == "368")
+    #expect(String(describing: Day12(data: bigTest).part2()) == "1206")
   }
 }

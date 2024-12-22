5b5ba64abcc974c5a51cb16caba1118f0fca8903
Evan <evan@brucemcrooster.dev>
2024-12-22 10:24:32 -0500

    Completed Day 22 Part 2 Sloooowly It took 2 minutes to run on my data, and almost 12 seconds to finish the tests

--- Sources/Day22.swift
+++ Sources/Day22.swift
@@ -9,17 +9,18 @@ struct Day22: AdventDay {
     self.data = data
     self.initialSecretNumbers = data.split(separator: "\n").compactMap { Int(String($0)) }
   }
-
+  
+  func increment(secretNumber: Int) -> Int {
+    let pruneMod = 16777216
+    
+    let step1 = ((secretNumber * 64) ^ secretNumber) % pruneMod
+    let step2 = ((step1 / 32) ^ step1) % pruneMod
+    let step3 = ((step2 * 2048) ^ step2) % pruneMod
+    
+    return step3
+  }
+  
   func part1() -> Any {
-    func increment(secretNumber: Int) -> Int {
-      let pruneMod = 16777216
-      
-      let step1 = ((secretNumber * 64) ^ secretNumber) % pruneMod
-      let step2 = ((step1 / 32) ^ step1) % pruneMod
-      let step3 = ((step2 * 2048) ^ step2) % pruneMod
-      
-      return step3
-    }
     
     var secretNumbers: [Int] = initialSecretNumbers
     
@@ -30,6 +31,100 @@ struct Day22: AdventDay {
   }
 
   func part2() -> Any {
-    return 0
+    func getPrices(forSecretNumber secretNumber: Int) -> [Int8] {
+      var currentNumber = secretNumber
+      var prices: [Int8] = [Int8(currentNumber % 10)]
+      
+      for _ in 1...2000 {
+        currentNumber = increment(secretNumber: currentNumber)
+        
+        prices.append(Int8(currentNumber % 10))
+      }
+      
+      assert(prices.count == 2001, "Prices array should have been 2001 elements (initial price plus 2000 changes), was instead \(prices.count) elements.")
+      
+      return prices
+    }
+    
+    func getPriceChanges(forPrices prices: [Int8]) -> [Int8?] {
+      let result: [Int8?] = try! prices.reduce<[Int8?]>(into: [nil]) { partialResult, nextElement in
+        if let previousElement = partialResult.last! {
+          partialResult.removeLast()
+          partialResult.append(nextElement - previousElement)
+        }
+        partialResult.append(nextElement)
+      }
+      
+      // The last element will still be kicking around
+      assert(result.count - 1 == prices.count)
+      return result.dropLast()
+    }
+    
+    func calcPricesAndChanges(forInitialSecretNumber startingSecret: Int) -> [(price: Int8, change: Int8)] {
+      let prices = getPrices(forSecretNumber: startingSecret)
+      let priceChanges = getPriceChanges(forPrices: prices)
+      
+      assert(prices.count == priceChanges.count)
+      
+      let priceChangesWithoutFirst = priceChanges.compactMap({ $0 })
+      
+      assert(priceChangesWithoutFirst.count == prices.count - 1)
+      
+      // We can ignore the first price and price change because the monkey could never stop there
+      
+      let result: [(price: Int8, change: Int8)] = zip(prices.dropFirst(), priceChangesWithoutFirst).map ({ $0 })
+      
+      return result
+    }
+    
+    struct PriceChangePattern: Hashable {
+      let change1: Int8
+      let change2: Int8
+      let change3: Int8
+      let change4: Int8
+      
+      func getSalePrice(inChanges priceChanges: [(price: Int8, change: Int8)]) -> Int? {
+        for index in priceChanges.startIndex..<(priceChanges.endIndex - 3) {
+          if priceChanges[index].change == self.change1
+              && priceChanges[index + 1].change == self.change2
+              && priceChanges[index + 2].change == self.change3
+              && priceChanges[index + 3].change == self.change4 {
+            return Int(priceChanges[index + 3].price)
+          }
+        }
+        return nil
+      }
+      
+      init(fromChanges priceChanges: [(price: Int8, change: Int8)], afterIndex startIndex: Int) {
+        assert(startIndex < priceChanges.endIndex - 3)
+        
+        self.change1 = priceChanges[startIndex].change
+        self.change2 = priceChanges[startIndex + 1].change
+        self.change3 = priceChanges[startIndex + 2].change
+        self.change4 = priceChanges[startIndex + 3].change
+      }
+    }
+    
+    var changesMaxPrice = [PriceChangePattern : Int]()
+    
+    assert(Set(initialSecretNumbers).count == initialSecretNumbers.count)
+    
+    let priceChanges: [[(price: Int8, change: Int8)]] = initialSecretNumbers.map(calcPricesAndChanges)
+    
+    for priceChange in priceChanges {
+      for startIndex in priceChange.startIndex..<(priceChange.endIndex - 3) {
+        let pattern = PriceChangePattern(fromChanges: priceChange, afterIndex: startIndex)
+        
+        guard changesMaxPrice[pattern] == nil else { continue }
+        
+        let maxPrice = priceChanges
+          .compactMap({ pattern.getSalePrice(inChanges: $0) })
+          .reduce(0, +)
+        
+        changesMaxPrice[pattern] = maxPrice
+      }
+    }
+    
+    return changesMaxPrice.values.max()!
   }
 }
--- Tests/Day22.swift
+++ Tests/Day22.swift
@@ -11,12 +11,20 @@ struct Day22Tests {
   2024
   
   """
+  
+  let partTwoTestData = """
+  1
+  2
+  3
+  2024
+  
+  """
 
   @Test func testPart1() async throws {
     #expect(String(describing: Day22(data: testData).part1()) == "37327623")
   }
 
   @Test func testPart2() async throws {
-    #expect(true)
+    #expect(String(describing: Day22(data: partTwoTestData).part2()) == "23")
   }
 }
